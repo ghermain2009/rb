18c7ec233bdf46bc256836da9c2ca28f5bc2997d
Administrador <Administrador@GermanTapia-HP.cruzdelsur.ecs>
2015-05-12 14:36:52 -0500

    liquidacion tcs

--- module/Dashboard/src/Dashboard/Model/CupcuponTable.php
+++ module/Dashboard/src/Dashboard/Model/CupcuponTable.php
@@ -297,7 +297,7 @@ class CupcuponTable {
             'precio_total'
         ))->from('cup_cupon')
         ->where(array('cup_cupon.id_estado_compra' => '5'))
-        ->where->addPredicate(new Between('fecha_validacion', new Expression("STR_TO_DATE('".$fecha." 00:00:00','%d/%m/%Y %H:%i:%s')"), new Expression("STR_TO_DATE('".$fecha." 23:59:59','%d/%m/%Y %H:%i:%s')")));
+        ->where->addPredicate(new Between('fecha_validacion', new Expression("STR_TO_DATE('".$fecha." 00:00:00','%d/%m/%Y %H:%i:%s')"), new Expression("NOW()")));
 
         $select->order(array('cup_cupon.id_campana', 'cup_cupon.id_campana_opcion', 'cup_cupon.fecha_validacion'));
         
