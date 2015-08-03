e57cdc53ba0b4bc2228b503da5afc00c4944c28c
Administrador <Administrador@GermanTapia-HP.cruzdelsur.ecs>
2015-08-02 20:54:43 -0500

    cambios url y adaptacion - ultimo

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -76,7 +76,7 @@ class IndexController extends AbstractActionController
         
         $pais = $config['id_pais'];
         $capital = $config['id_capital'];
-        $existepromocion = false; 
+        $existepromocion = true; 
         
         $departamentoTable = $serviceLocator->get('Dashboard\Model\UbidepartamentoTable');
         $departamentos = $departamentoTable->getDepartamentosxPais($pais);
