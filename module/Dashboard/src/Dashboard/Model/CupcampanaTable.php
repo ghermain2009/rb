6d7528a5e6211070019c6e4853d17ad1c37a5bf2
Administrador <Administrador@GermanTapia-HP.cruzdelsur.ecs>
2015-09-17 23:58:43 -0500

    cambios url y adaptacion - ultimo

--- module/Dashboard/src/Dashboard/Model/CupcampanaTable.php
+++ module/Dashboard/src/Dashboard/Model/CupcampanaTable.php
@@ -13,6 +13,7 @@ use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Expression;
 use Zend\Authentication\AuthenticationService;
 use Zend\Stdlib\ArrayUtils;
+use Zend\Db\Sql\Predicate\Between;
 /**
  * Description of CupcampanaTable
  *
@@ -56,8 +57,8 @@ class CupcampanaTable {
                       'precio_especial'  => new Expression("MIN(precio_especial)") ,
                       'vendidos'  => new Expression("SUM(IFNULL(vendidos,0))") ,
                       'descuento'  => new Expression("100-ROUND(MIN(precio_especial)*100/MIN(precio_regular))") ,
-                    ));
-        //->where->addPredicate(new Between('FECHA_EMISION_DOC', new Expression("TO_DATE('".$fecha." 00:00:00','YYYY-MM-DD HH24:MI:SS')"), new Expression("TO_DATE('".$fecha." 23:59:59','YYYY-MM-DD HH24:MI:SS')")));
+                    ))
+        //->where->addPredicate(new Between(new Expression("NOW()"), new Expression("CONCAT(DATE_FORMAT(cup_campana.fecha_inicio,'%Y-%m-%d'),' ',cup_campana.hora_inicio)"), new Expression("CONCAT(DATE_FORMAT(cup_campana.fecha_final,'%Y-%m-%d'),' ',cup_campana.hora_final)")));
         
         $select->group(array('cup_campana.id_campana'));
         $select->group(array('cup_campana.subtitulo'));
