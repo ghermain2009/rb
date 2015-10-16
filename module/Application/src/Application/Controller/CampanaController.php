945a73f727f29b6ceb7edf94a8483ad045bcaff2
German Tapia <gtapia@gibarcena.com.pe>
2015-10-16 13:21:36 -0500

    mobil afiliacion

--- module/Application/src/Application/Controller/CampanaController.php
+++ module/Application/src/Application/Controller/CampanaController.php
@@ -191,8 +191,8 @@ class CampanaController extends AbstractActionController {
         $clienteTable->addCliente($datos);
 
         $cuponTable = $serviceLocator->get('Dashboard\Model\CupcuponTable');
-        //$idTransaccion = $cuponTable->addCupon($datos,$serviceLocator);
-        $idTransaccion = 171;
+        $idTransaccion = $cuponTable->addCupon($datos,$serviceLocator);
+        //$idTransaccion = 171;
         
         
         $config = $serviceLocator->get('config');
@@ -301,7 +301,8 @@ class CampanaController extends AbstractActionController {
                 $acquirerId = $id_adquirer;
                 $idCommerce = $id_commerce;
                 $purchaseOperationNumber = $idTransaccion;
-                $purchaseAmount = str_replace('.','',$datos['PriceTotal']);
+                //$purchaseAmount = str_replace('.','',$datos['PriceTotal']);
+                $purchaseAmount = '080';
                 $purchaseCurrencyCode = '840'; //DOLARES AMERICANOS
                 
                 $claveSecretaVpos = $datosPayme['clave_vpos'];
